17bf3877a85b094c236dfd270e9eb8bb56e35aa0
tyamada <7885034+onaipevoli@users.noreply.github.com>
2017-11-06 14:59:15 +0900

    Script updating

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-ln -sf ~/dotfiles/.emacs ~/.emacs
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.less ~/.less
-ln -sf ~/dotfiles/.lesskey ~/.lesskey
-ln -sf ~/dotfiles/.login ~/.login
-ln -sf ~/dotfiles/.screenrc ~/.screenrc
-ln -sf ~/dotfiles/.tcshrc ~/.tcshrc
+repository=~/dotfiles
+
+targets=(
+    .login
+    .tcshrc
+    .emacs
+    .gitconfig
+    .less
+    .lesskey
+    .screenrc
+)
+
+for target in "${targets[@]}"; do
+    ln -sf $repository/$target ~/$target
+done
